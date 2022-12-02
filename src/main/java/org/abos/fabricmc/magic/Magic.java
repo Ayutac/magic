ddceafbceceb19b960198de3f823540991aea8d7
ayutac <fly.high.android@gmail.com>
2022-12-02 17:47:31 +0100

    first wand lol

--- src/main/java/org/abos/fabricmc/magic/Magic.java
+++ src/main/java/org/abos/fabricmc/magic/Magic.java
@@ -1,6 +1,10 @@
 package org.abos.fabricmc.magic;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,11 +12,13 @@ public class Magic implements ModInitializer {
 
     public final static String MOD_ID = "magic";
 
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public final static Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public final static Wand BEGINNER_WAND = new Wand(new FabricItemSettings());
 
     @Override
     public void onInitialize() {
-
+        Registry.register(Registries.ITEM, new Identifier(MOD_ID, "beginner_wand"), BEGINNER_WAND);
         LOGGER.info("Initializing the Magic Mod...");
     }
 }
---
+++ src/main/java/org/abos/fabricmc/magic/Wand.java
@@ -0,0 +1,10 @@
+package org.abos.fabricmc.magic;
+
+import net.minecraft.item.Item;
+
+public class Wand extends Item {
+
+    public Wand(Settings settings) {
+        super(settings);
+    }
+}
