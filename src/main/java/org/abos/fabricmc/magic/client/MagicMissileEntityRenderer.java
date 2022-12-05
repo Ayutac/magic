7e28fab87ac91ee2bb8d97e60867837d3aefe6e6
ayutac <fly.high.android@gmail.com>
2022-12-05 18:57:12 +0100

    added different texture

--- src/main/java/org/abos/fabricmc/magic/client/MagicMissileEntityRenderer.java
+++ src/main/java/org/abos/fabricmc/magic/client/MagicMissileEntityRenderer.java
@@ -3,6 +3,7 @@ package org.abos.fabricmc.magic.client;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.ProjectileEntityRenderer;
 import net.minecraft.util.Identifier;
+import org.abos.fabricmc.magic.Magic;
 import org.abos.fabricmc.magic.entities.MagicMissileEntity;
 
 public class MagicMissileEntityRenderer<T extends MagicMissileEntity> extends ProjectileEntityRenderer<T> {
@@ -13,6 +14,6 @@ public class MagicMissileEntityRenderer<T extends MagicMissileEntity> extends Pr
 
     @Override
     public Identifier getTexture(T entity) {
-        return new Identifier("textures/entity/projectiles/arrow.png");
+        return new Identifier(Magic.MOD_ID, "textures/entities/projectiles/small_earth_missile.png");
     }
 }
