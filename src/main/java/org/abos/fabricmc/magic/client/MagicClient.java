4145fa679f84da78f4c02673c4def8f8f5bd4b42
ayutac <fly.high.android@gmail.com>
2022-12-11 18:51:41 +0100

    kind of added something resembling a bar

--- src/main/java/org/abos/fabricmc/magic/client/MagicClient.java
+++ src/main/java/org/abos/fabricmc/magic/client/MagicClient.java
@@ -2,6 +2,13 @@ package org.abos.fabricmc.magic.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.abos.fabricmc.magic.Magic;
 import org.abos.fabricmc.magic.MagicContent;
 
 public class MagicClient implements ClientModInitializer {
@@ -19,5 +26,40 @@ public class MagicClient implements ClientModInitializer {
         EntityRendererRegistry.register(MagicContent.SMALL_WATER_MISSILE_ENTITY_TYPE, context -> new MagicMissileEntityRenderer<>(context, "water_missile.png"));
         EntityRendererRegistry.register(MagicContent.MEDIUM_WATER_MISSILE_ENTITY_TYPE, context -> new MagicMissileEntityRenderer<>(context, "water_missile.png"));
         EntityRendererRegistry.register(MagicContent.BIG_WATER_MISSILE_ENTITY_TYPE, context -> new MagicMissileEntityRenderer<>(context, "water_missile.png"));
+
+        HudRenderCallback.EVENT.register(new HudRenderCallback() {
+            @Override
+            public void onHudRender(MatrixStack matrixStack, float tickDelta) {
+                PlayerEntity player = MinecraftClient.getInstance().player;
+                if (player != null) {
+                    int manaMax = Magic.MANA.get(player).getMax();
+                    int mana = Magic.MANA.get(player).getValue();
+                    int k = MinecraftClient.getInstance().getWindow().getScaledHeight() - 39;
+                    int l = MinecraftClient.getInstance().getWindow().getScaledWidth() / 2 + 91;
+                    int m = k;
+                    int n = 0;
+
+                    for(boolean bl = false; manaMax > 0; n += 20) {
+                        int o = Math.min(manaMax, 10);
+                        manaMax -= o;
+
+                        for(int p = 0; p < o; ++p) {
+                            int r = 0;
+                            int s = l - p * 8 - 9;
+                            DrawableHelper.drawTexture(matrixStack, s, m, 0, (float)52 + r * 9, (float)9, 9, 9, 256, 256);
+                            if (p * 2 + 1 + n < mana) {
+                                DrawableHelper.drawTexture(matrixStack, s, m, 0, (float)88, (float)9, 9, 9, 256, 256);
+                            }
+
+                            if (p * 2 + 1 + n == mana) {
+                                DrawableHelper.drawTexture(matrixStack, s, m, 0, (float)97, (float)9, 9, 9, 256, 256);
+                            }
+                        }
+
+                        m -= 10;
+                    }
+                }
+            }
+        });
     }
 }
