5ff935aeaa7b53fce81378e7c4899eb626202b75
ayutac <fly.high.android@gmail.com>
2022-12-14 21:05:56 +0100

    refactored the pillar raising out

--- src/main/java/org/abos/fabricmc/magic/entities/EarthPillarProjectileEntity.java
+++ src/main/java/org/abos/fabricmc/magic/entities/EarthPillarProjectileEntity.java
@@ -1,19 +1,15 @@
 package org.abos.fabricmc.magic.entities;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
 import org.abos.fabricmc.magic.MagicContent;
 import org.abos.fabricmc.magic.utils.MissileSize;
-
-import java.util.List;
+import org.abos.fabricmc.magic.utils.WorldUtils;
 
 public class EarthPillarProjectileEntity extends MagicProjectileEntity {
 
@@ -37,34 +33,9 @@ public class EarthPillarProjectileEntity extends MagicProjectileEntity {
     protected void applyBlockEffects(BlockHitResult blockHitResult) {
         super.applyBlockEffects(blockHitResult);
         BlockPos blockPos = blockHitResult.getBlockPos();
-        BlockState blockState = world.getBlockState(blockPos);
-        BlockPos targetPos = blockPos;
-        if (blockState.isIn(MagicContent.EARTH_PILLAR_TARGETS)) {
-            if (isReplaceableByPillar(targetPos.up())) {
-                targetPos = targetPos.up();
-                world.setBlockState(targetPos, blockState);
-                if (isReplaceableByPillar(targetPos.up())) {
-                    targetPos = targetPos.up();
-                    world.setBlockState(targetPos, blockState);
-                    if (isReplaceableByPillar(targetPos.up())) {
-                        targetPos = targetPos.up();
-                        world.setBlockState(targetPos, blockState);
-                    }
-                }
-            }
+        if (world.getBlockState(blockPos).isIn(MagicContent.EARTH_PILLAR_TARGETS)) {
+            WorldUtils.raiseBlock(world, blockPos, 3);
         }
-        targetPos = targetPos.up();
-        if (!targetPos.equals(blockPos.up())) {
-            List<Entity> entities = world.getNonSpectatingEntities(Entity.class, new Box(0d,1d,0d,1d,2d,1d).offset(blockPos));
-            for (Entity entity : entities) {
-                BlockPos diff = targetPos.subtract(blockPos.up());
-                entity.setPos(entity.getX(), entity.getY() + diff.getY(), entity.getZ());
-            }
-        }
-    }
-
-    private boolean isReplaceableByPillar(BlockPos blockPos) {
-        return world.getBlockState(blockPos).isAir() || !world.getFluidState(blockPos).isEmpty();
     }
 
     public static EarthPillarProjectileEntity create(World world, PlayerEntity user) {
---
+++ src/main/java/org/abos/fabricmc/magic/utils/WorldUtils.java
@@ -0,0 +1,47 @@
+package org.abos.fabricmc.magic.utils;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public class WorldUtils {
+
+    private WorldUtils() {
+        /* No instantiation. */
+    }
+
+    public static void raiseBlock(final World world, final BlockPos groundPos, final int height) {
+        if (height < 0) {
+            throw new IllegalArgumentException("Height must be non-negative!");
+        }
+        if (height == 0) {
+            return;
+        }
+        BlockState groundState = world.getBlockState(groundPos);
+        BlockPos targetPos = groundPos;
+        int actualHeight = 0;
+        for (; actualHeight < height; actualHeight++) {
+            if (!isReplaceable(world, targetPos.up())) {
+                break;
+            }
+            targetPos = targetPos.up();
+            world.setBlockState(targetPos, groundState);
+        }
+        targetPos = targetPos.up();
+        if (!targetPos.equals(groundPos.up())) {
+            List<Entity> entities = world.getNonSpectatingEntities(Entity.class, new Box(0d,1d,0d,1d,2d,1d).offset(groundPos));
+            for (Entity entity : entities) {
+                BlockPos diff = targetPos.subtract(groundPos.up());
+                entity.setPos(entity.getX(), entity.getY() + diff.getY(), entity.getZ());
+            }
+        }
+    }
+
+    public static boolean isReplaceable(final World world, final BlockPos blockPos) {
+        return world.getBlockState(blockPos).isAir() || !world.getFluidState(blockPos).isEmpty();
+    }
+}
