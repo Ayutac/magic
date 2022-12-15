3cec5de859f964585aa1034c8b8edd729b44dbe6
ayutac <fly.high.android@gmail.com>
2022-12-15 13:16:23 +0100

    added datagen structure

---
+++ src/datagen/groovy/org/abos/fabricmc/magic/datagen/MagicDataGeneratorEntry.groovy
@@ -0,0 +1,13 @@
+package org.abos.fabricmc.magic.datagen
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+class MagicDataGeneratorEntry implements DataGeneratorEntrypoint {
+
+    @Override
+    void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+
+}
